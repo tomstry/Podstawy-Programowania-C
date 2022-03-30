25a56e8b22624062d6182c46bfbaae0e41a2853a
Tomasz Stryjak <stryjak.tomasz@gmail.com>
2022-03-30 17:02:33 +0200

    task 1 and 2 completed

--- lab_2/2d_array.c
+++ lab_2/2d_array.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 #include<stdio.h>
 #include<time.h>
-#define N 8
+#define N 2
 
-void randomise(int array[N][N],int min,int max){
+void randomise(int array[N][N]){
     for(int i = 0; i < N; i++){
         for(int j = 0; j < N;j++){
-            array[i][j] = (rand() % (max - min +1)) + min;
+            array[i][j] = (random() % 11);
         }
     }
 
@@ -14,49 +14,68 @@ void randomise(int array[N][N],int min,int max){
 void print_array(int array[N][N]){
     for(int i=0; i < N; i++){
         for(int j=0; j < N; j++){
-            printf("%d ", array[i][j]);
+            printf("%-2d ", array[i][j]);
         }
         printf("\n");
     }
+    printf("\n");
 }
 int sum(int array[N][N]){
     int przek = 0;
     int suma = 0;
     int col = 0;
 
-    for(int i = 0; i < 8; i++){
+    for(int i = 0; i < N; i++){
         col = 0;
-        for(int j = 0; j < 8; j++){
+        for(int j = 0; j < N; j++){
             suma = suma + array[i][j];
             if(i == j){
                 przek = przek + array[i][j];
             }
             col = col + array[j][i];
         }
-        printf("Col%d: %d ",i,col);
+        printf("Col%d: %d ",i+1,col);
     }
-    
+    printf("\nPrzekątna: %d", przek);
+    printf("\nWszystkie: %d\n\n", suma);
 }
 
+void matrix_multiplicate(int array1[N][N], int array2[N][N]){
+    int multi_array[N][N];
 
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            multi_array[i][j] = 0;
+        }
+    }
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            for(int k = 0; k < N; k++){
+                multi_array[i][j] += array1[i][k] * array2[k][j]; 
+            }
+        }
+    }
+    print_array(multi_array);
+}
 
 int main(){
 
-    srand(time(NULL));
+    srandom(time(NULL));
 
     int tab1[N][N];
     int tab2[N][N];
 
-    randomise(tab1[N][N],0,10);
- //   print_array(tab1[N][N]);
-    
-    for(int i=0; i < N; i++){
-        for(int j=0; j < N; j++){
-            printf("%d ", tab1[i][j]);
-        }
-        printf("\n");
-    }
+    randomise(tab1);
+    randomise(tab2);
 
+    printf("tab1 : \n\n");
+    print_array(tab1);
+
+    printf("tab2 : \n\n");
+    print_array(tab2);
+    sum(tab1);
+    printf("Wynik mnożenia: \n\n");
+    matrix_multiplicate(tab1,tab2);
 
 
 
